54863f3a088ff6e3695313cffb56688eba4681bf
walterwhiteman <yashnegim4u@gmail.com>
2025-06-02 10:17:19 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,8 @@ import { setupVite, serveStatic, log } from "./vite";
 import cors from 'cors';
 
 // --- NEW IMPORT FOR DATABASE CONNECTION ---
-import { Pool } from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 // --- END NEW IMPORT ---
 
 const app = express();
