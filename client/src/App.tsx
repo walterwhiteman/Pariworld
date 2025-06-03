952a6964a5fa093dcaa263361aba318782135feb
walterwhiteman <yashnegim4u@gmail.com>
2025-06-03 21:24:53 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,18 @@
-import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ChatPage from "@/pages/chat"; // Ensure this import is correct
 
-function Router() {
-  return (
-    <Switch>
-      {/* Main chat page */}
-      <Route path="/" component={ChatPage} />
-      
-      {/* Fallback - redirect to chat */}
-      <Route>
-        <ChatPage />
-      </Route>
-    </Switch>
-  );
-}
-
 function App() {
+  console.log('[App.tsx Render] App component rendering...'); // ADDED: Log every render
+
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        {/* MODIFIED: Directly render ChatPage, removing wouter for now */}
+        <ChatPage />
       </TooltipProvider>
     </QueryClientProvider>
   );
