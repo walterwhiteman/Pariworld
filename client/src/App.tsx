29efb22400286f024552f36b7d0e3e52805db47d
walterwhiteman <yashnegim4u@gmail.com>
2025-06-04 13:57:54 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ChatPage from "@/pages/chat";
-// This import will now correctly resolve to src/hooks/useSocket.tsx after the rename.
 import { SocketProvider } from '@/hooks/useSocket';
 import React from 'react';
 
