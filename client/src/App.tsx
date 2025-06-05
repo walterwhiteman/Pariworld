75c91476828054c70869069250a7f628d6b64c4d
walterwhiteman <yashnegim4u@gmail.com>
2025-06-05 13:57:42 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "wouter";
+import { Route, Switch } from "wouter"; // <-- Changed 'Routes' to 'Switch'
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -7,13 +7,14 @@ import ChatPage from "@/pages/chat";
 
 function Router() {
   return (
-    <Routes>
+    <Switch> {/* <-- Changed <Routes> to <Switch> */}
       {/* Main chat page */}
       <Route path="/" component={ChatPage} />
       
-      {/* Fallback - redirect to chat */}
-      <Route path="*" component={ChatPage} />
-    </Routes>
+      {/* Fallback - redirect to chat (or show a 404/default) */}
+      {/* In wouter, a catch-all route without a path will match if no other route matches */}
+      <Route component={ChatPage} /> {/* <-- No 'path' for fallback/catch-all */}
+    </Switch>
   );
 }
 
