5280baebe61f35c6c37938668b8bd20ba7a4471b
walterwhiteman <yashnegim4u@gmail.com>
2025-06-05 12:47:02 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Routes, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -7,15 +7,13 @@ import ChatPage from "@/pages/chat";
 
 function Router() {
   return (
-    <Switch>
+    <Routes>
       {/* Main chat page */}
       <Route path="/" component={ChatPage} />
       
       {/* Fallback - redirect to chat */}
-      <Route>
-        <ChatPage />
-      </Route>
-    </Switch>
+      <Route path="*" component={ChatPage} />
+    </Routes>
   );
 }
 
